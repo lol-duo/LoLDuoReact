07365ce33efbcc47e402825ed9adf9465cb02fea
imygnam <imygnam@gmail.com>
2022-08-02 19:38:00 +0900

    검색 기능 추가 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+import { useState } from 'react';
 import './css/App.css';
 import './css/Main.css';
 import championListData from './static/championList.json'
 
 function App() {
+  const ChosungSearch = require('hangul-chosung-search-js');
+  const [championName, setChampionName] = useState('');
+  const onChange = (e) =>{
+    setChampionName(e.target.value);
+  }  
   const championListli = championListData.map(c => {
-    return (
-      <li className='Champion'>
-        <img src={c.imgUrl} alt={c.imgUrl} width='46px' height='46px'></img>
-      </li>
-    );
-  })
-
+    return(
+       ChosungSearch.isSearch(championName, c.name) && 
+          <li className='Champion'>
+            <img src={c.imgUrl} alt={c.imgUrl} width='46px' height='46px'></img>
+          </li>
+    )
+  }) 
 
   return (
     <div className="App">
       <header  className="App-header">
+        <div>
          <div className='Main-content'>
-            <div className='Sub-content ChampionList'>
-              {championListli}
+            <div className='Sub-content-ChampionList'>
+              <div>
+                <input id="filterChampion" type="text" placeholder="챔피언 검색 (가렌, ㄱㄹ, ...)" value={championName} onChange={onChange}/>
+              </div>
+              <div className="ChampionList">
+                {championListli}
+              </div>
+            </div>            
+            <div className='Sub-content WinRateList'>
             </div>
-          </div>          
+          </div>  
+        </div>        
       </header>
     </div>
   );
