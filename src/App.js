04af5631088b6bd0a824545b4766f76b8e91e843
imygnam <imygnam@gmail.com>
2022-08-03 17:22:58 +0900

    유저 선택시 이미지 반영

--- src/App.js
+++ src/App.js
@@ -2,22 +2,59 @@ import { useState } from 'react';
 import './css/App.css';
 import './css/Main.css';
 import championListData from './static/championList.json'
+import lineDate from './static/line.json'
+
+ 
+
+const lineListImg =
+  <div>
+    <img src={lineDate.ALL} alt={lineDate.ALL}></img>
+    <img src={lineDate.TOP} alt={lineDate.TOP}></img>
+    <img src={lineDate.JUNGLE} alt={lineDate.JUNGLE}></img>
+    <img src={lineDate.MIDDLE} alt={lineDate.MIDDLE}></img>
+    <img src={lineDate.BOTTOM} alt={lineDate.BOTTOM}></img>
+    <img src={lineDate.UTILITY} alt={lineDate.UTILITY}></img>
+  </div>
+
 
 function App() {
-  const ChosungSearch = require('hangul-chosung-search-js');
+  const [userSelected, setUserSelected] = useState([{"id" : 0, "line" : "ALL"}]);
   const [championName, setChampionName] = useState('');
-  const onChange = (e) =>{
+  const [selected , setSelected] = useState(0);
+  const ChosungSearch = require('hangul-chosung-search-js');
+
+  const onChangeSelected = (e) => {
+    setSelected(e);
+  }
+  //유저가 클릭한 곳에 champion id 넣기.
+  const onChangeUserSelected = (e) => {
+    userSelected[selected].id = e;
+    setUserSelected(Object.assign([{}], userSelected));
+  }
+
+  const onChangeName = (e) =>{
     setChampionName(e.target.value);
-  }  
+  }
+
   const championListli = championListData.map(c => {
     return(
-       ChosungSearch.isSearch(championName, c.name) && 
+      ChosungSearch.isSearch(championName, c.name) && 
           <li className='Champion'>
-            <img src={c.imgUrl} alt={c.imgUrl} width='46px' height='46px'></img>
+            <img src={c.imgUrl} alt={c.imgUrl} width='46px' height='46px' onClick={() => onChangeUserSelected(c.id)}></img>
           </li>
     )
-  }) 
+  })
 
+  const userSelectedSpace = userSelected.map(s => {
+    return(
+      <li className='UserSelected'>
+        <img src={championListData.find(champion => champion.id === s.id).imgUrl}></img>
+        <img src={lineDate[s.line]}></img>
+      </li>
+    )
+  })
+  
+  
   return (
     <div className="App">
       <header  className="App-header">
@@ -25,8 +62,12 @@ function App() {
          <div className='Main-content'>
             <div className='Sub-content-ChampionList'>
               <div>
-                <input id="filterChampion" type="text" placeholder="챔피언 검색 (가렌, ㄱㄹ, ...)" value={championName} onChange={onChange}/>
+                {userSelectedSpace}
               </div>
+              <div>
+                <input id="filterChampion" type="text" placeholder="챔피언 검색 (가렌, ㄱㄹ, ...)" value={championName} onChange={onChangeName}/>
+              </div>
+                {lineListImg}
               <div className="ChampionList">
                 {championListli}
               </div>
