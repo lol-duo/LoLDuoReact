205e838d50fcb8571b80d310a910d642d83c7a16
imygnam <imygnam@gmail.com>
2022-08-29 22:40:45 +0900

    test

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Detatil from './Detail';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
