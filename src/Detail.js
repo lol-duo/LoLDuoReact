2e65f795ab8ca3b7730f152793b4800ec3a931de
imygnam <imygnam@gmail.com>
2022-08-30 23:50:52 +0900

    test

--- src/Detail.js
+++ src/Detail.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useLocation } from "react-router-dom";
 import env from "./static/env.json";
 
+
 function Detatil() {
   const { state } = useLocation();
   const [selectedPerkChampionId, setSelectedPerkChampionId] = useState(2);
